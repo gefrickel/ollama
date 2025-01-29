9a41a08426b435c87087a56c2f0bf145d1771c61
gefrickel <54885382+gefrickel@users.noreply.github.com>
2025-01-29 14:16:18 +0100

    Update push_docker.sh

--- scripts/push_docker.sh
+++ scripts/push_docker.sh
@@ -5,7 +5,7 @@ set -eu
 export VERSION=${VERSION:-0.0.0}
 export GOFLAGS="'-ldflags=-w -s \"-X=github.com/ollama/ollama/version.Version=$VERSION\" \"-X=github.com/ollama/ollama/server.mode=release\"'"
 
-docker build \
+podman build \
     --push \
     --platform=linux/arm64,linux/amd64 \
     --build-arg=VERSION \
